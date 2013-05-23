8d06894af488c89d8b9e9226326ccebac2aa2844
Mark Tutkowski <tutkowskim@msoe.edu>
2013-05-23 19:01:52 +0200

    Screen Structure Implemented and functional.

---
+++ SMVDriverDisplay/src/edu/smv/android/HelpActivity.java
@@ -0,0 +1,31 @@
+package edu.smv.android;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+
+
+public class HelpActivity extends Activity {
+	
+	/**
+	 * Method gets run when the activity is created.
+	 * @param savedInstanceState
+	 */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        this.setContentView(R.layout.activity_help);
+    }
+
+    
+    /**
+     * Method gets run when creating the options menu
+     * @param menu
+     * @return
+     */
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.activity_main, menu);
+        return true;
+    }
+}
