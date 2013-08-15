3acda39fc43a0f9e3e36ce7b8820612e0c02485c
tutkowskim <tutkowskim@msoe.edu>
2013-08-14 21:02:50 -0500

    Temp cape API written.

---
+++ SMV_Data_Acquisition_and_Management/src/edu/smv/cape/Cape.java
@@ -0,0 +1,15 @@
+package edu.smv.cape;
+
+
+/**
+ * A cape is an expansion board for a beaglebone using it's IO connections.
+ * This class is an api for a custom cape for our beaglebone black.
+ * 
+ * "Capes arn't just for super heros anymore!"
+ * 
+ * @author Mark
+ *
+ */
+public class Cape {
+
+}
