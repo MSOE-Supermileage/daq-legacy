25d48ac85b88d3d96d12d99cf303eb610d761d7d
Mark Tutkowski <tutkowskim@msoe.edu>
2013-12-07 14:20:29 -0600

    Encoder templete.

--- Data Display & Managment/SMV_Data_Acquisition_and_Management/src/edu/smv/dam/PI/Encoder.java
+++ Data Display & Managment/SMV_Data_Acquisition_and_Management/src/edu/smv/dam/PI/Encoder.java
@@ -1,5 +1,19 @@
 package edu.smv.dam.PI;
 
-public class Encoder {
+import java.util.Date;
 
+public class Encoder {
+    private double shaftCircumference;
+    private Date lastInterupt;
+    private Date secoundToLastInterupt;
+    
+    
+    public double getTimeBetweenInterupts(){
+        return 0;
+    }
+    
+    
+    public double getShaftSpeed(){
+        return 0;
+    }
 }
